df74eb19d164a35d57192b81a17d3eddec35c840
ChanMinLee2 <dlcksals87@gmail.com>
2024-07-27 20:15:35 +0900

    Feat: api 인스턴스 두 개 모두 사용

--- FE/src/api/axiosInstance.js
+++ FE/src/api/axiosInstance.js
@@ -16,15 +16,17 @@ const axiosApi = (url) => {
 };
 
 // post, delete등 api요청 시 인증값이 필요한 경우
-// const axiosAuthApi = (url, options) => {
-//   const token = "토큰 값";
-//   const instance = axios.create({
-//     baseURL: url,
-//     headers: { Authorization: "Bearer " + token },
-//     ...options,
-//   });
-//   return instance;
-// };
+const axiosAuthApi = (url, options) => {
+  const token = import.meta.env.VITE_UNIMADE_SELLER_API_KEY;
+  const instance = axios.create({
+    baseURL: url,
+    headers: {
+      Authorization: "Bearer " + token,
+    },
+    ...options,
+  });
+  return instance;
+};
 
 export const defaultInstance = axiosApi(BASE_URL);
-// export const authInstance = axiosAuthApi(BASE_URL);
+export const authInstance = axiosAuthApi(BASE_URL);
